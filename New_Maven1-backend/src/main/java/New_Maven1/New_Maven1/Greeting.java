28d7ece319fdb7a8a7765d741205ec48f8d8eed4
abc <pradnyaaug@gmail.com>
2024-03-26 06:41:55 +0530

    Pradnya "This is first project"

---
+++ New_Maven1-backend/src/main/java/New_Maven1/New_Maven1/Greeting.java
@@ -0,0 +1,21 @@
+package New_Maven1.New_Maven1;
+
+public class Greeting {
+
+    private final long id;
+
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ New_Maven1-backend/src/test/java/New_Maven1/New_Maven1/Hello.java
@@ -0,0 +1,10 @@
+package New_Maven1.New_Maven1;
+
+public class Hello 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+
+}
